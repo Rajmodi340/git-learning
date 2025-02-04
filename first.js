2124db8d483c6f60c16e490766ebf5b957053623
Raj Modi <modiraj340@gmail.com>
2025-02-04 18:05:37 +0530

    bugfix

--- first.js
+++ first.js
@@ -1,13 +1,5 @@
-// git reset history ko delete kr deta h
-// git revert <hashcode> history ko maintain krta h
-// console.log("hello")
-// const balance=20
-// function update(){
-// balance+=10
-// }
-// update()
-// console.log("money")
-// console.log("bug commit")
+
+
 const button ="added a button"
 console.log(button)
 const login="login page added"
